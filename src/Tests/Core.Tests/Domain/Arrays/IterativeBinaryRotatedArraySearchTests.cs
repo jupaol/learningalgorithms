7537eeaeed8d5d1555cd8efc02971f41210168fd
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-09 02:28:35 -0700

    Adding examples to search a rotated array using binary search

---
+++ src/Tests/Core.Tests/Domain/Arrays/IterativeBinaryRotatedArraySearchTests.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Domain.Arrays;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Arrays
+{
+	[TestClass]
+	public class IterativeBinaryRotatedArraySearchTests
+	{
+		[TestClass]
+		public class TheSearchMethod
+		{
+			[TestMethod]
+			public void It_should_search_a_value_in_a_right_rotated_array_using_binary_search()
+			{
+				var fixture = new Fixture().Customize(new AutoMoqCustomization());
+				var sut = fixture.Create<IterativeBinaryRotatedArraySearch>();
+				int res;
+				var source = new[] { 69, 80, 100, 1, 2, 4, 7, 8, 12, 15, 19, 24, 50 };
+
+				for (int i = 0; i < source.Length; i++)
+				{
+					res = sut.Search(source, source[i], 3, RotationType.Right);
+					res.Should().Be(i);
+				}
+
+				res = sut.Search(source, -4, 3, RotationType.Right);
+				res.Should().Be(-1);
+
+				res = sut.Search(source, 400, 3, RotationType.Right);
+				res.Should().Be(-1);
+
+				res = sut.Search(source, 6, 3, RotationType.Right);
+				res.Should().Be(-1);
+			}
+
+			[TestMethod]
+			public void It_should_search_a_value_in_a_left_rotated_array_using_binary_search()
+			{
+				var fixture = new Fixture().Customize(new AutoMoqCustomization());
+				var sut = fixture.Create<IterativeBinaryRotatedArraySearch>();
+				int res;
+				var source = new[] { 7, 8, 12, 15, 19, 24, 50, 69, 80, 100, 1, 2, 4 };
+
+				for (int i = 0; i < source.Length; i++)
+				{
+					res = sut.Search(source, source[i], 3, RotationType.Left);
+					res.Should().Be(i);
+				}
+
+				res = sut.Search(source, -4, 3, RotationType.Left);
+				res.Should().Be(-1);
+
+				res = sut.Search(source, 400, 3, RotationType.Left);
+				res.Should().Be(-1);
+
+				res = sut.Search(source, 6, 3, RotationType.Left);
+				res.Should().Be(-1);
+			}
+		}
+	}
+}
