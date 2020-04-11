f430ce99ab4074a58406d507d115d9f481eca693
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-11 00:08:00 -0700

    Adding Insert/Merge sorting algorithms

---
+++ src/Core/Domain/Arrays/Sort/InsertSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Core.Domain.Arrays.Sort
+{
+	public class InsertSort
+	{
+		public T[] Sort<T>(T[] source)
+			where T : IComparable<T>
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (source.Length == 0)
+			{
+				return Array.Empty<T>();
+			}
+
+			for (int i = 1; i < source.Length; i++)
+			{
+				T item = source[i];
+				int hole = i;
+
+				while (hole > 0 && source[hole - 1].CompareTo(item) > 0)
+				{
+					source[hole] = source[hole - 1];
+					hole--;
+				}
+
+				source[hole] = item;
+			}
+
+			return source;
+		}
+	}
+}
---
+++ src/Core/Domain/Arrays/Sort/MergeSort.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Core.Domain.Arrays.Sort
+{
+	public class MergeSort
+	{
+		public T[] Sort<T>(T[] source)
+			where T : IComparable<T>
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (source.Length == 0)
+			{
+				return Array.Empty<T>();
+			}
+
+			DoSort(source);
+
+			return source;
+		}
+
+		private void DoSort<T>(T[] source)
+			where T : IComparable<T>
+		{
+			if (source.Length <= 1)
+			{
+				return;
+			}
+
+			int mid = source.Length / 2;
+
+			T[] left = new T[mid];
+			T[] right = new T[source.Length - mid];
+
+			for (int i = 0; i < left.Length; i++)
+			{
+				left[i] = source[i];
+			}
+
+			for (int i = mid; i < source.Length; i++)
+			{
+				right[i - mid] = source[i];
+			}
+
+			DoSort(left);
+			DoSort(right);
+
+			Merge(left, right, source);
+		}
+
+		private void Merge<T>(T[] left, T[] right, T[] source)
+			where T : IComparable<T>
+		{
+			int leftIndex = 0;
+			int rightIndex = 0;
+			int targetIndex = 0;
+
+			while (leftIndex < left.Length && rightIndex < right.Length && targetIndex < source.Length)
+			{
+				if (left[leftIndex].CompareTo(right[rightIndex]) <= 0)
+				{
+					source[targetIndex] = left[leftIndex];
+					targetIndex++;
+					leftIndex++;
+				}
+				else
+				{
+					source[targetIndex] = right[rightIndex];
+					targetIndex++;
+					rightIndex++;
+				}
+			}
+
+			while (leftIndex < left.Length)
+			{
+				source[targetIndex] = left[leftIndex];
+				targetIndex++;
+				leftIndex++;
+			}
+
+			while (rightIndex < right.Length)
+			{
+				source[targetIndex] = right[rightIndex];
+				targetIndex++;
+				rightIndex++;
+			}
+		}
+	}
+}
