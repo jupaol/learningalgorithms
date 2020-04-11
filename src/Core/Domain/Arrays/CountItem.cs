ea567709540976b623d90bf97104dc287f36d901
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-11 02:34:16 -0700

    Adding algorith to count number of items in a sorted array

---
+++ src/Core/Domain/Arrays/CountItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Core.Domain.Arrays.Search;
+
+namespace Core.Domain.Arrays
+{
+	public class CountItem
+	{
+		private readonly BinarySearchIndexesSimplified _binarySearchIndexesSimplified;
+
+		public CountItem(BinarySearchIndexesSimplified binarySearchIndexesSimplified)
+		{
+			_binarySearchIndexesSimplified = binarySearchIndexesSimplified;
+		}
+
+#pragma warning disable S3900 // Arguments of public methods should be validated against null
+		public int Count<T>(T[] source, T key)
+			where T : IComparable<T>
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (source.Length == 0)
+			{
+				return 0;
+			}
+
+			int min = _binarySearchIndexesSimplified.Search(source, key, OccurrenceType.First);
+
+			if (min == -1)
+			{
+				return 0;
+			}
+
+			int max = _binarySearchIndexesSimplified.Search(source, key, OccurrenceType.Last);
+
+			return max - min + 1;
+		}
+#pragma warning restore S3900 // Arguments of public methods should be validated against null
+	}
+}
---
+++ src/Tests/Core.Tests/Domain/Arrays/CountItemTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Domain.Arrays;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Arrays
+{
+	[TestClass]
+	public class CountItemTests
+	{
+		[TestClass]
+		public class TheCountMethod
+		{
+			[TestMethod]
+			public void It_should_count_the_number_of_elements()
+			{
+				IFixture fixture = new Fixture().Customize(new AutoMoqCustomization());
+				var sut = fixture.Create<CountItem>();
+				var source = new[] { 4, 6, 6, 6, 8, 9, 9, 9, 10, 10, 11 };
+				int res;
+
+				foreach (var item in source)
+				{
+					res = sut.Count(source, item);
+					res.Should().Be(source.Count(x => x == item));
+				}
+
+				res = sut.Count(source, -2);
+				res.Should().Be(0);
+
+				res = sut.Count(source, 300);
+				res.Should().Be(0);
+
+				res = sut.Count(source, 5);
+				res.Should().Be(0);
+			}
+		}
+	}
+}
