ca957cd4d272bb2ccc16086e2e51ab94692fd240
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-13 23:47:06 -0700

    Adding quick sort descending

---
+++ src/Core/Domain/Arrays/Sort/QuickSortRandomizedDescendingRecursive.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace Core.Domain.Arrays.Sort
+{
+	public static class QuickSortRandomizedDescendingRecursive
+	{
+		private static readonly Random _random = new Random((int)DateTime.Now.Ticks);
+
+		public static ILearningCollection<T> SortUsingQuickSortRandomizedDescendingRecursive<T>(
+			this ILearningCollection<T> source)
+			where T : IComparable<T>
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			T[] arr = source.ToArray();
+
+			DoSort(arr, 0, arr.Length - 1);
+
+			return new LearningCollection<T>(arr);
+		}
+
+		private static void DoSort<T>(T[] arr, int min, int max)
+			where T : IComparable<T>
+		{
+			if (min >= max)
+			{
+				return;
+			}
+
+			int randomIndex = _random.Next(min, max);
+
+			Swap(arr, max, randomIndex);
+
+			int pivotIndex = GetPivot(arr, min, max);
+
+			DoSort(arr, min, pivotIndex - 1);
+			DoSort(arr, pivotIndex + 1, max);
+		}
+
+		private static int GetPivot<T>(T[] arr, in int min, in int max)
+			where T : IComparable<T>
+		{
+			int index = min;
+			T pivot = arr[max];
+
+			for (int i = min; i < max; i++)
+			{
+				if (arr[i].CompareTo(pivot) >= 0)
+				{
+					Swap(arr, i, index);
+					index++;
+				}
+			}
+
+			Swap(arr, index, max);
+
+			return index;
+		}
+
+		private static void Swap<T>(T[] arr, in int index1, in int index2)
+			where T : IComparable<T>
+		{
+			T tmp = arr[index1];
+
+			arr[index1] = arr[index2];
+			arr[index2] = tmp;
+		}
+	}
+}
---
+++ src/Tests/Core.Tests/Domain/Arrays/Sort/QuickSortRandomizedDescendingRecursiveTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Domain;
+using Core.Domain.Arrays.Sort;
+using Core.Domain.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Arrays.Sort
+{
+	[TestClass]
+	public class QuickSortRandomizedDescendingRecursiveTests
+	{
+		[TestClass]
+		public class TheSortUsingQuickSortRandomizedDescendingRecursiveMethod
+		{
+			[TestMethod]
+			public void It_should_sort_an_array_using_randomized_quick_sort()
+			{
+				ILearningCollection<int> res;
+				int[] source;
+				ILearningCollection<int> sut;
+
+				source = new[] { 5, 3, 9, 2, 0, -3, 5 };
+				sut = source.AsLearningCollection();
+				res = sut.SortUsingQuickSortRandomizedDescendingRecursive();
+				res.Should().ContainInOrder(source.OrderByDescending(x => x));
+
+				source = new[] { -3, 0, 2, 3, 5, 5, 9 };
+				sut = source.AsLearningCollection();
+				res = sut.SortUsingQuickSortRandomizedDescendingRecursive();
+				res.Should().ContainInOrder(source.OrderByDescending(x => x));
+			}
+		}
+	}
+}
