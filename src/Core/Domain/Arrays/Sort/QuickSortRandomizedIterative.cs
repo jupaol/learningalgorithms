8c897d03334ff8a8b9dd5d66bd3930e2f49782a3
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-14 00:43:42 -0700

    QuickSort iterative

---
+++ src/Core/Domain/Arrays/Sort/QuickSortRandomizedIterative.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Domain.Arrays.Sort
+{
+	public static class QuickSortRandomizedIterative
+	{
+		public static ILearningCollection<T> SortUsingQuickSortRandomizedIterative<T>(
+			this ILearningCollection<T> source)
+			where T : IComparable<T>
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			T[] arr = source.ToArray();
+
+			DoSort(arr, 0, arr.Length - 1);
+
+			return new LearningCollection<T>(arr);
+		}
+
+		private static void DoSort<T>(T[] arr, int min, int max)
+			where T : IComparable<T>
+		{
+			var stack = new Stack<int>();
+
+			stack.Push(min);
+			stack.Push(max);
+
+			while (stack.Count > 0)
+			{
+				int right = stack.Pop();
+				int left = stack.Pop();
+
+				int pivot = GetPivot(arr, left, right);
+
+				if (pivot - 1 > left)
+				{
+					stack.Push(left);
+					stack.Push(pivot - 1);
+				}
+
+				if (pivot + 1 < right)
+				{
+					stack.Push(pivot + 1);
+					stack.Push(right);
+				}
+			}
+		}
+
+		private static int GetPivot<T>(T[] arr, in int min, in int max)
+			where T : IComparable<T>
+		{
+			int index = min;
+			T pivot = arr[max];
+
+			for (int i = min; i < max; i++)
+			{
+				if (arr[i].CompareTo(pivot) <= 0)
+				{
+					Swap(arr, i, index);
+					index++;
+				}
+			}
+
+			Swap(arr, index, max);
+
+			return index;
+		}
+
+		private static void Swap<T>(T[] arr, in int i1, in int i2)
+			where T : IComparable<T>
+		{
+			T tmp = arr[i1];
+
+			arr[i1] = arr[i2];
+			arr[i2] = tmp;
+		}
+	}
+}
---
+++ src/Tests/Core.Tests/Domain/Arrays/Sort/QuickSortRandomizedIterativeTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Domain;
+using Core.Domain.Arrays.Sort;
+using Core.Domain.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Arrays.Sort
+{
+	[TestClass]
+	public class QuickSortRandomizedIterativeTests
+	{
+		[TestClass]
+		public class TheSortUsingQuickSortRandomizedIterativeMethod
+		{
+			[TestMethod]
+			public void It_should_sort_an_array_using_randomized_quick_sort()
+			{
+				ILearningCollection<int> sut;
+				ILearningCollection<int> res;
+				int[] source;
+
+				source = new[] { 5, 3, 9, 2, 0, -3, 5 };
+				sut = source.AsLearningCollection();
+				res = sut.SortUsingQuickSortRandomizedIterative();
+				res.Should().ContainInOrder(source.OrderBy(x => x));
+
+				source = new[] { -3, 0, 2, 3, 5, 5, 9 };
+				sut = source.AsLearningCollection();
+				res = sut.SortUsingQuickSortRandomizedIterative();
+				res.Should().ContainInOrder(source.OrderBy(x => x));
+			}
+		}
+	}
+}
