ac82516dd707905bc44674990648d788c2ce784a
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-18 18:12:46 -0700

    Adding permutations tests

---
+++ src/Core/Domain/Permutations/AllPermutationsExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Core.Domain.Permutations
+{
+	public static class AllPermutationsExtensions
+	{
+		public static string[] GetAllStringPermutationsUsingRecursion(
+			this string source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			var list = new List<string>();
+
+			GetAllPermutations(string.Empty, source, list);
+
+			return list.ToArray();
+		}
+
+		public static IEnumerable<IEnumerable<T>> GetAllPermutationsUsingRecursion<T>(
+			this IEnumerable<T> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			T[] a = source.ToArray();
+			var list = new List<T[]>();
+
+			GetAllPermutations(a, 0, list);
+
+			return list;
+		}
+
+		private static void GetAllPermutations<T>(T[] a, int start, ICollection<T[]> list)
+		{
+			if (start >= a.Length)
+			{
+				list.Add((T[])a.Clone());
+
+				return;
+			}
+
+			for (int i = start; i < a.Length; i++)
+			{
+				Swap(a, i, start);
+				GetAllPermutations(a, start + 1, list);
+				Swap(a, i, start);
+			}
+		}
+
+		private static void GetAllPermutations(string prefix, string suffix, IList<string> list)
+		{
+			if (string.IsNullOrWhiteSpace(suffix))
+			{
+				list.Add(prefix);
+				return;
+			}
+
+			for (int i = 0; i < suffix.Length; i++)
+			{
+				GetAllPermutations(
+					prefix + suffix[i].ToString(CultureInfo.InvariantCulture),
+					suffix.Substring(0, 0 + i) + suffix.Substring(i + 1),
+					list);
+			}
+		}
+
+		private static void Swap<T>(T[] a, int i1, int i2)
+		{
+			T tmp = a[i1];
+
+			a[i1] = a[i2];
+			a[i2] = tmp;
+		}
+	}
+}
---
+++ src/Tests/Core.Tests/Domain/Permutations/AllPermutationsExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Domain.Permutations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Permutations
+{
+	[TestClass]
+	public class AllPermutationsExtensionsTests
+	{
+		private static int GetFactorial(int n)
+		{
+			int res = 1;
+
+			for (int i = 0; i < n; i++)
+			{
+				res *= n - i;
+			}
+
+			return res;
+		}
+
+		[TestClass]
+		public class TheGetAllStringPermutationsUsingRecursionMethod
+		{
+			[TestMethod]
+			public void It_should_get_all_permutations_from_a_string()
+			{
+				string source;
+				string[] res;
+
+				source = "bad";
+				res = source.GetAllStringPermutationsUsingRecursion();
+				res.Length.Should().Be(GetFactorial(source.Length));
+				res.Should().ContainInOrder("bad", "bda", "abd", "adb", "dba", "dab");
+			}
+		}
+
+		[TestClass]
+		public class TheGetAllPermutationsUsingRecursionMethod
+		{
+			[TestMethod]
+			public void It_should_get_all_permutations_from_an_array()
+			{
+				int[] source;
+				int[][] res;
+
+				source = new[] { 1, 2, 3 };
+				res = source.GetAllPermutationsUsingRecursion().Select(x => x.ToArray()).ToArray();
+				res.Length.Should().Be(GetFactorial(source.Length));
+				res[0].Should().ContainInOrder(new[] { 1, 2, 3 });
+				res[1].Should().ContainInOrder(new[] { 1, 3, 2 });
+				res[2].Should().ContainInOrder(new[] { 2, 1, 3 });
+				res[3].Should().ContainInOrder(new[] { 2, 3, 1 });
+				res[4].Should().ContainInOrder(new[] { 3, 2, 1 });
+				res[5].Should().ContainInOrder(new[] { 3, 1, 2 });
+			}
+		}
+	}
+}
