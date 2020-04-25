0c4095c5d6a32dcc9bdc77e1aafd01abb45b5320
Juan Pablo Olmos Lara <jupaol@hotmail.com>
2020-04-25 04:38:30 -0700

    Adding more permutations examples

---
+++ src/Core/Domain/Permutations/AllPermutationsOfBalancedBracersExtensions.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Domain.Permutations
+{
+	public static class AllPermutationsOfBalancedBracersExtensions
+	{
+		public static IEnumerable<IEnumerable<char>> GetAllBalancedBracersPermutations(
+			this int n)
+		{
+			var res = new List<char[]>();
+
+			GetAllBalancedBracersPermutationsRec(
+				0,
+				0,
+				n,
+				'{',
+				'}',
+				new List<char>(),
+				res);
+
+			return res;
+		}
+
+		public static IEnumerable<IEnumerable<char>> GetAllBalancedBracersPermutationsUsingCounts(
+			this int n)
+		{
+			var res = new List<char[]>();
+			char[] choices = new char[2];
+			int[] counts = new int[2];
+
+			choices[0] = '{';
+			choices[1] = '}';
+
+			counts[0] = n;
+			counts[1] = n;
+
+			GetAllBalancedBracersPermutationsUsingCountsRec(
+				choices, counts, new List<char>(), res);
+
+			return res;
+		}
+
+		private static void GetAllBalancedBracersPermutationsRec(
+			int leftCount,
+			int rightCount,
+			in int n,
+			char openBracer,
+			char closeBracer,
+			IList<char> currentCombination,
+			ICollection<char[]> res)
+		{
+			if (leftCount >= n && rightCount >= n)
+			{
+				res.Add(currentCombination.ToArray());
+			}
+
+			if (leftCount < n)
+			{
+				currentCombination.Add(openBracer);
+				GetAllBalancedBracersPermutationsRec(
+					leftCount + 1, rightCount, n, openBracer, closeBracer, currentCombination, res);
+				currentCombination.RemoveAt(currentCombination.Count - 1);
+			}
+
+			if (rightCount < leftCount)
+			{
+				currentCombination.Add(closeBracer);
+				GetAllBalancedBracersPermutationsRec(
+					leftCount, rightCount + 1, n, openBracer, closeBracer, currentCombination, res);
+				currentCombination.RemoveAt(currentCombination.Count - 1);
+			}
+		}
+
+		private static void GetAllBalancedBracersPermutationsUsingCountsRec(
+			char[] choices,
+			int[] counts,
+			IList<char> currentPermutation,
+			ICollection<char[]> res)
+		{
+			if (counts.All(x => x == 0) && AreBracersBalanced(currentPermutation))
+			{
+				res.Add(currentPermutation.ToArray());
+			}
+
+			for (int i = 0; i < choices.Length; i++)
+			{
+				if (counts[i] == 0)
+				{
+					continue;
+				}
+
+				counts[i]--;
+				currentPermutation.Add(choices[i]);
+
+				GetAllBalancedBracersPermutationsUsingCountsRec(
+					choices, counts, currentPermutation, res);
+
+				counts[i]++;
+				currentPermutation.RemoveAt(currentPermutation.Count - 1);
+			}
+		}
+
+		private static bool AreBracersBalanced(IEnumerable<char> bracers)
+		{
+			if (!bracers.Any() || bracers.Count() % 2 != 0)
+			{
+				return false;
+			}
+
+			var stack = new Stack<char>();
+
+			foreach (char c in bracers)
+			{
+				if (IsOpenedBracer(c))
+				{
+					stack.Push(c);
+				}
+				else if (IsClosedBracer(c))
+				{
+					if (stack.Count == 0)
+					{
+						return false;
+					}
+
+					if (IsBracerMatch(stack.Peek(), c))
+					{
+						stack.Pop();
+					}
+					else
+					{
+						stack.Push(c);
+					}
+				}
+				else
+				{
+					throw new Exception("Invalid character found");
+				}
+			}
+
+			return stack.Count == 0;
+		}
+
+		private static bool IsOpenedBracer(char c)
+		{
+			return new[] { '{', '(', '[' }.Contains(c);
+		}
+
+		private static bool IsClosedBracer(char c)
+		{
+			return new[] { '}', '(', ']' }.Contains(c);
+		}
+
+		private static bool IsBracerMatch(char open, char close)
+		{
+			var dic = new Dictionary<char, char>
+			{
+				{ '{', '}' },
+				{ '(', ')' },
+				{ '[', ']' }
+			};
+
+			return
+				IsOpenedBracer(open)
+				&& IsClosedBracer(close)
+				&& dic[open] == close;
+		}
+	}
+}
---
+++ src/Tests/Core.Tests/Domain/Permutations/AllPermutationsOfBalancedBracersExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Domain.Permutations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Domain.Permutations
+{
+	[TestClass]
+	public class AllPermutationsOfBalancedBracersExtensionsTests
+	{
+		[TestClass]
+		public class TheGetAllBalancedBracersPermutationsMethod
+		{
+			[TestMethod]
+			public void It_should_get_all_the_balanced_bracers_permutations()
+			{
+				int source;
+				IEnumerable<char>[] res;
+
+				source = 3;
+				res = source.GetAllBalancedBracersPermutations().ToArray();
+				res.Should().HaveCount(5);
+				res[0].Should().ContainInOrder('{', '{', '{', '}', '}', '}');
+				res[1].Should().ContainInOrder('{', '{', '}', '{', '}', '}');
+				res[2].Should().ContainInOrder('{', '{', '}', '}', '{', '}');
+				res[3].Should().ContainInOrder('{', '}', '{', '{', '}', '}');
+				res[4].Should().ContainInOrder('{', '}', '{', '}', '{', '}');
+			}
+		}
+
+		[TestClass]
+		public class TheGetAllBalancedBracersPermutationsUsingCountsMethod
+		{
+			[TestMethod]
+			public void It_should_get_all_the_balanced_bracers_permutations()
+			{
+				int source;
+				IEnumerable<char>[] res;
+
+				source = 3;
+				res = source.GetAllBalancedBracersPermutationsUsingCounts().ToArray();
+				res.Should().HaveCount(5);
+				res[0].Should().ContainInOrder('{', '{', '{', '}', '}', '}');
+				res[1].Should().ContainInOrder('{', '{', '}', '{', '}', '}');
+				res[2].Should().ContainInOrder('{', '{', '}', '}', '{', '}');
+				res[3].Should().ContainInOrder('{', '}', '{', '{', '}', '}');
+				res[4].Should().ContainInOrder('{', '}', '{', '}', '{', '}');
+			}
+		}
+	}
+}
